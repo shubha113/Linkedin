8e6a3191144a7a5fd3b4a5d6fdf03e691f0d5694
shubha113 <85664043+shubha113@users.noreply.github.com>
2025-08-03 21:40:13 +0530

    Update api.js

--- linkedin-frontend/src/utils/api.js
+++ linkedin-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://linkedin-1ab1.onrender.com',
   withCredentials: true,
 });
 
@@ -16,3 +16,4 @@ api.interceptors.request.use(
 );
 
 export default api;
+
